6cf724fa12af1ae5dd50d721ccc6e1a251380425
Sami Spets <savasp@utu.fi>
2019-02-21 00:10:55 +0200

    added socket.io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
-const path =require('path');
-const express = require('express'),
-	  app = express();
+const path =require('path'),
+	  http = require('http'),
+	  express = require('express'),
+	  app = express(),
+	  socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-app.use(express.static(publicPath))
 
+app.use(express.static(publicPath));
+const server = http.createServer(app);
+const io = socketIO(server);
 
-app.listen(port, (req, res) => {
-	console.log(`Serve is up in port ${port}`)
+io.on('connection', (socket) => {
+	socket.on('disconnect', () => {
+		console.log('client disconnected')
+	})
+})
+
+server.listen(port, (req, res) => {
+	console.log(`Server is up in port ${port}`)
 })
