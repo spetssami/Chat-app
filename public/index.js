081a336d245907c0ee2526430b4cd0f8c2aa3435
Sami Spets <savasp@utu.fi>
2019-02-27 00:09:11 +0200

    Added autoscroll feature

--- public/index.js
+++ public/index.js
@@ -1,5 +1,31 @@
 const socket = io();
 
+
+let hasLastMessage = false;
+function scrollToBottom() {
+	const messages = document.getElementById('messages');
+	const newMessage = messages.lastElementChild
+
+	const clientHeight = messages.clientHeight
+	const scrollTop = messages.scrollTop
+	const scrollHeight= messages.scrollHeight
+	const newMessageHeight = turnIntoNumber(newMessage)
+	let lastMessageHeight;
+	if(hasLastMessage){
+		lastMessageHeight = turnIntoNumber(newMessage.previousElementSibling)
+	} 
+	hasLastMessage = true;
+	if(scrollHeight <= clientHeight + scrollTop + newMessageHeight + lastMessageHeight){
+		messages.scrollTo(0, scrollHeight)
+	}
+}
+
+turnIntoNumber = (str) => {
+	const hasPixels = window.getComputedStyle(str, null).getPropertyValue("height")
+	const number = parseInt(hasPixels.substring(0, hasPixels.length - 2));
+	return number;
+}
+
 socket.on('connect', () => {
 	console.log('connected')
 
@@ -17,7 +43,8 @@ socket.on('newUser', (message) => {
 		from: message.from,
 		createdAt: creationTime
 	})
-	document.getElementById('messages').innerHTML += html
+	document.getElementById('messages').innerHTML += html;
+	scrollToBottom();
 	// const formattedTime = moment(message.createdAt).format('HH:mm')
 	// let li = document.createElement('LI')
 	// li.innerHTML = `${message.from} ${formattedTime}: ${message.text}`
