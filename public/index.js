bc82d4f618bd15089b76d98dfa29c7fa1c5e1195
Sami Spets <savasp@utu.fi>
2019-03-01 00:17:31 +0200

    Some minor changes in code

--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@ const socket = io();
 
 
 let hasLastMessage = false;
-function scrollToBottom() {
+scrollToBottom = () => {
 	const messages = document.getElementById('messages');
 	const newMessage = messages.lastElementChild
 
@@ -11,18 +11,15 @@ function scrollToBottom() {
 	const scrollHeight= messages.scrollHeight
 	const newMessageHeight = turnIntoNumber(newMessage)
 	let lastMessageHeight;
-	if(hasLastMessage){
-		lastMessageHeight = turnIntoNumber(newMessage.previousElementSibling)
-	} 
-	hasLastMessage = true;
+	hasLastMessage ? (lastMessageHeight = turnIntoNumber(newMessage.previousElementSibling)) : (hasLastMessage = true)
 	if(scrollHeight <= clientHeight + scrollTop + newMessageHeight + lastMessageHeight){
 		messages.scrollTo(0, scrollHeight)
 	}
 }
 
 turnIntoNumber = (str) => {
-	const hasPixels = window.getComputedStyle(str, null).getPropertyValue("height")
-	const number = parseInt(hasPixels.substring(0, hasPixels.length - 2));
+	const string = window.getComputedStyle(str, null).getPropertyValue("height")
+	const number = parseInt(string.substring(0, string.length - 2));
 	return number;
 }
 
@@ -45,13 +42,9 @@ socket.on('newUser', (message) => {
 	})
 	document.getElementById('messages').innerHTML += html;
 	scrollToBottom();
-	// const formattedTime = moment(message.createdAt).format('HH:mm')
-	// let li = document.createElement('LI')
-	// li.innerHTML = `${message.from} ${formattedTime}: ${message.text}`
-	// document.getElementById('messages').appendChild(li)
 })
 
-let messageForm = document.getElementById('message-form')
+const messageForm = document.getElementById('message-form')
 
 messageForm.addEventListener("submit", (event) => {
 	event.preventDefault();
